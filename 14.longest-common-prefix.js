38b32aad9c6534f273198868ddbc7b1216970b69
manunidhi999 <manunidhichinthireddy@gmail.com>
2025-11-28 17:15:19 +0530

    longest-common-prefix

---
+++ 14.longest-common-prefix.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=14 lang=javascript
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return "";
+
+    let prefix = strs[0]; 
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1); 
+            if (!prefix) return ""; 
+        }
+    }
+    return prefix;
+};
+    
+
