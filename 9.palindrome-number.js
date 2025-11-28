2849b554c2334c5497c44af88abb3f5636fb4e90
manunidhi999 <manunidhichinthireddy@gmail.com>
2025-11-28 14:42:59 +0530

    palindrome number

---
+++ 9.palindrome-number.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=9 lang=javascript
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    const s = x.toString();
+    const rev = s.split('').reverse().join('');
+    return s === rev;
+};
