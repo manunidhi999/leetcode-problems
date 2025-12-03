1468f67f695153da9005911f4597819ad7708f82
manunidhi999 <manunidhichinthireddy@gmail.com>
2025-12-03 19:52:15 +0530

    Find-the-Index-of-the-First-Occurrence-in-a-String

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=28 lang=javascript
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+// @lc code=start
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    const n = haystack.length;
+    const m = needle.length;
+
+    for (let i = 0; i <= n - m; i++) {
+        let j = 0;
+        while (j < m && haystack[i + j] === needle[j]) {
+            j++;
+        }
+        if (j === m) return i;  
+    }
+
+    return -1;  
+};
+
