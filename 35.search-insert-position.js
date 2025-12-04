42574c5f1777dbd78ac85433116b67d6a62220f3
manunidhi999 <manunidhichinthireddy@gmail.com>
2025-12-04 14:30:42 +0530

    search insert position

---
+++ 35.search-insert-position.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=35 lang=javascript
+ *
+ * [35] Search Insert Position
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if(target < nums[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+
+    }
+    return left;
+    
+};
+
