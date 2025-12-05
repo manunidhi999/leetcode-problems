22655eab89241f388b9515d612b6f95647ea77b4
manunidhi999 <manunidhichinthireddy@gmail.com>
2025-12-05 16:35:17 +0530

    length-of-last-word

---
+++ 58.length-of-last-word.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=58 lang=javascript
+ *
+ * [58] Length of Last Word
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    s = s.trim();
+    let lastIndex = s.lastIndexOf(" ");
+    return s.length - lastIndex - 1;
+};
