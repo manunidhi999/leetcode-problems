eeacafeaee8f9be5bcc4d850eb7ce0ec02a48ab3
manunidhi999 <manunidhichinthireddy@gmail.com>
2025-12-08 15:35:01 +0530

    plus-one

---
+++ 66.plus-one.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=66 lang=javascript
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+function plusOne(digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i] += 1;
+      return digits;
+    }
+    digits[i] = 0;
+  }
+  digits.unshift(1);
+  return digits;
+}
