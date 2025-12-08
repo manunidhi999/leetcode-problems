30402ac070857bd44df9b5db3678271d5afbb59c
manunidhi999 <manunidhichinthireddy@gmail.com>
2025-12-08 18:18:40 +0530

    add-binary

--- 67.add-binary.js
+++ 67.add-binary.js
@@ -10,20 +10,25 @@
  * @param {string} b
  * @return {string}
  */
+/**
+ * Add two binary strings and return their sum as a binary string.
+ * Works for very long strings because it never converts the whole string to a number.
+ */
 var addBinary = function(a, b) {
-     let i = a.length - 1;
-     let j = b.length - 1;
-     let carry = 0;
-     let result = [];
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    const res = [];
 
-     while (i >= 0 || carry) {
+    while (i >= 0 || j >= 0 || carry) {
         let sum = carry;
-        if (i >= 0) sum += Number(a[i--]);
-        if (j >= 0) sum += Number(b[j--]);
 
-        result.push(sum % 2);
-        carry = Math.floor (sum / 2);
-     }
-     return result.reverse().join("");
-    
+        if (i >= 0) sum += a[i--] === '1' ? 1 : 0;
+        if (j >= 0) sum += b[j--] === '1' ? 1 : 0;
+
+        res.push((sum % 2).toString());
+        carry = (sum >> 1);
+    }
+
+    return res.reverse().join('');
 };
