6070768b0abd22dcbee01549ca3bf238d4d105f2
manunidhi999 <manunidhichinthireddy@gmail.com>
2025-12-15 17:55:37 +0530

    binary-tree-inorder-traversal

---
+++ 94.binary-tree-inorder-traversal.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=94 lang=javascript
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    let result = [];
+    function dfs(node) {
+        if(!node) return;
+
+        dfs(node.left);
+        result.push(node.val);
+        dfs(node.right);
+    }
+    dfs(root);
+    return result;
+    
+};
+
