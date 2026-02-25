6d68cdb4db4a2fe7cd2906bccf36fec7d7b841fa
manunidhi999 <manunidhichinthireddy@gmail.com>
2026-02-25 17:02:31 +0530

    linked-list-cycle.js

---
+++ 141.linked-list-cycle.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=141 lang=javascript
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if (!head || !head.next) return false;
+
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+        slow = slow.next;          
+        fast = fast.next.next;     
+
+        if (slow === fast) {
+            return true;           
+        }
+    }
+
+    return false;                 
+};
+
+
+// @lc code=end
+
