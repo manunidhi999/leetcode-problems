5a9e505486babcf4a4efa6679fd81fba6e777b44
manunidhi999 <manunidhichinthireddy@gmail.com>
2026-03-05 17:10:04 +0530

    binary-tree-preorder-traversal.js

---
+++ 144.binary-tree-preorder-traversal.kt
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=144 lang=kotlin
+ *
+ * [144] Binary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun preorderTraversal(root: TreeNode?): List<Int> {
+        
+    }
+}
+// @lc code=end
+
