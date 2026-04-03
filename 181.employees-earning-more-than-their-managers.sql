b04300c82fdac4d1ef84b5af92ca1d6a263cf7b1
manunidhi999 <manunidhichinthireddy@gmail.com>
2026-04-03 14:35:39 +0530

    employees-earning-more-than-their-manager.sql

---
+++ 181.employees-earning-more-than-their-managers.sql
@@ -0,0 +1,16 @@
+--
+-- @lc app=leetcode id=181 lang=mysql
+--
+-- [181] Employees Earning More Than Their Managers
+--
+
+-- @lc code=start
+# Write your MySQL query statement below
+SELECT e.name AS Employee
+FROM Employee e
+JOIN Employee m
+ON e.managerId = m.id
+WHERE e.salary > m.salary;
+
+-- @lc code=end
+
